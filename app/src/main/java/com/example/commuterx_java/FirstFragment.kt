74057e09697a877413dc1b3cd2145fa4ea6f2dc9
shivermetimbers <143888364+shivermetimbers65@users.noreply.github.com>
2024-10-13 10:25:15 +0800

    Fixed the username not recording for Google SSO and polished the UI of FullScreenMap and FirstFragment

--- app/src/main/java/com/example/commuterx_java/FirstFragment.kt
+++ app/src/main/java/com/example/commuterx_java/FirstFragment.kt
@@ -10,13 +10,10 @@ import android.graphics.Canvas
 import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.Drawable
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.EditText
 import androidx.appcompat.widget.SearchView
 import android.widget.TextView
 import android.widget.Toast
--- app/src/main/java/com/example/commuterx_java/UsernameFragment.kt
+++ app/src/main/java/com/example/commuterx_java/UsernameFragment.kt
@@ -1,9 +1,6 @@
 package com.example.commuterx_java
 
-import android.app.Activity
-import android.content.Context
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,7 +8,6 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.auth.FirebaseAuth
